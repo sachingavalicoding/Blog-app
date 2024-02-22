144082185b03d13868579385c8f54d2f0467319f
Sachin Gavali <sachingavali321@gmail.com>
2024-02-23 01:00:06 +0530

    fetch blogs

--- src/pages/blogs/BlogLayout.jsx
+++ src/pages/blogs/BlogLayout.jsx
@@ -2,10 +2,32 @@ import { IoSearchSharp } from "react-icons/io5";
 import { AiFillLike } from "react-icons/ai";
 import { FaComment } from "react-icons/fa6";
 import "./blog.css";
-import { useState } from "react";
-import { posts } from "../../data";
+import { useState, useEffect } from "react";
+import { databases } from "../../server/appwrite";
 const BlogLayout = () => {
   const [search, setSearch] = useState("");
+  const [blogs, setBlogs] = useState([]);
+  console.log(blogs);
+  // Function to fetch blog data from Appwrite
+  const fetchBlogs = async () => {
+    try {
+      // Replace 'YOUR_COLLECTION_ID' with the actual collection ID where your blogs are stored in Appwrite
+      const documents = await databases.listDocuments(
+        "65c550a5cf78fda2ccfe",
+        "65c550b5bf7a160dfdce"
+      );
+
+      setBlogs(documents.documents);
+    } catch (error) {
+      console.error("Error fetching blogs:", error);
+    }
+  };
+
+  // useEffect to fetch blogs when the component mounts
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
     <>
       <div className="blog__container">
@@ -27,14 +49,14 @@ const BlogLayout = () => {
         </div>
 
         <ul className="blog__wrapper">
-          {posts.map(
-            ({ id, heading, subheading, author, likes, comments, image }) => {
+          {blogs.map(
+            ({ id, title, subheading, author, likes, comments, image }) => {
               return (
                 <li key={id} className="post">
                   <img src={image} alt="post" />
                   <div className="post__content">
                     <h3> {author} </h3>
-                    <h2> {heading} </h2>
+                    <h2> {title} </h2>
                     <p> {subheading} </p>
                     <div className="btn__container">
                       <button className="btn"> Read More </button>
